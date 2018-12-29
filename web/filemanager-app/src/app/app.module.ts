e95f9378f1565d14deb6727a650ad62ee7d189ec
wangxing <wangxing@yzsj.net.cn>
2018-12-29 09:05:32 +0800

    界面

--- web/filemanager-app/src/app/app.module.ts
+++ web/filemanager-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SourceComponent } from './source/source.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SourceComponent
   ],
   imports: [
     BrowserModule,
---
+++ web/filemanager-app/src/app/source/source.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-file-source',
+    templateUrl: 'source.component.html',
+    styleUrls: ['source.component.scss']
+})
+
+export class SourceComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
